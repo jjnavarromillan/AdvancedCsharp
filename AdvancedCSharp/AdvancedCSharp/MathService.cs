70a561aab7a56ffefc3595d146e41f8e5525333f
jjnavarromillan@gmail.com <jjnavarromillan@gmail.com>
2019-05-27 20:26:13 -0700

    Clear func<> and pass it directly as a datatype

--- AdvancedCSharp/AdvancedCSharp/MathService.cs
+++ AdvancedCSharp/AdvancedCSharp/MathService.cs
@@ -5,7 +5,6 @@ namespace AdvancedCSharp
 {
     public class MathService {
         public Action<double> MathPerformed;
-        public Func<double,double,double> CalculationHandler;
         public void CalculateNumbers(double value1,double value2, Func<double, double, double> calculation) {
             Timer timer = new Timer(5000);
             MathPerformed(calculation(value1,value2));

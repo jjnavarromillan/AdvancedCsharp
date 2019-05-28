448f0d7a6dc1e7d833f4e013dc603a2030d3fd86
jjnavarromillan@gmail.com <jjnavarromillan@gmail.com>
2019-05-27 20:25:21 -0700

    Convert delegates to a Func<> and Action<>

--- AdvancedCSharp/AdvancedCSharp/MathService.cs
+++ AdvancedCSharp/AdvancedCSharp/MathService.cs
@@ -4,10 +4,9 @@ using System.Timers;
 namespace AdvancedCSharp
 {
     public class MathService {
-        public delegate void MathPerformedHandler(double result);
-        public delegate double CalculationHandler(double value1, double value2);
-        public event MathPerformedHandler MathPerformed;
-        public void CalculateNumbers(double value1,double value2,CalculationHandler calculation) {
+        public Action<double> MathPerformed;
+        public Func<double,double,double> CalculationHandler;
+        public void CalculateNumbers(double value1,double value2, Func<double, double, double> calculation) {
             Timer timer = new Timer(5000);
             MathPerformed(calculation(value1,value2));
         }
--- AdvancedCSharp/AdvancedCSharp/Program.cs
+++ AdvancedCSharp/AdvancedCSharp/Program.cs
@@ -7,7 +7,7 @@ namespace AdvancedCSharp
         static void Main(string[] args)
         {
             MathService mathService = new MathService();
-            mathService.MathPerformed += (result) => Console.WriteLine("Calculatio result " + result));
+            mathService.MathPerformed += (result) => Console.WriteLine("Calculatio result " + result);
             mathService.CalculateNumbers(57.85, 789.76,(value1,value2)=> value1 * value2);
             
         }
